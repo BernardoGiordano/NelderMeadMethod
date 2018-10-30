ae303b54a1de7b5bb0ae7c0d2286b69c373b5088
BernardoGiordano <bernardogiordano369@gmail.com>
2018-10-30 23:23:53 +0100

    Backup

--- functionToApproximate.m
+++ functionToApproximate.m
@@ -3,14 +3,14 @@ clear all
 clc
 
 % coil radius [m]
-R = [1 1 1];
+R = [0.7 0.8 0.6];
 % coil current [A]
-I = [1 1 1];
+I = [0.3 0.5 0.2];
 % coil position on the z axis [m]
-Z = [1 1 1];
+Z = [0.4 0.9 1.4];
 
 % the actual script
-maxZ = max(Z)*4; % evaluate until 4 times the position of the last coil
+maxZ = max(Z)*2; % evaluate until 2 times the position of the last coil
 step = 0.01; % arbitrary
 z = -maxZ:step:maxZ; % independant variable
 bz = symmetricCoilMagneticField(R, I, Z, z);
@@ -33,7 +33,7 @@ function bz = symmetricCoilMagneticField(R, I, Z, z)
         error("Arrays must have the same size")
     end
 
-    mu0 = 4*pi*10e-7; % vacuum permeability [H/m]
+    mu0 = 4*pi*1e-7; % vacuum permeability [H/m]
     iterations = length(z); % how many iteration we need
     bz = zeros(iterations, 1); % output array, preallocating memory for efficiency
     
