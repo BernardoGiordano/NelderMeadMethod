ce4fa8e36c5e80018834bce772bc4fbd88964788
BernardoGiordano <bernardogiordano369@gmail.com>
2018-11-18 16:35:36 +0100

    Partial fixes

--- coils/test.m
+++ coils/test.m
@@ -12,30 +12,28 @@ hZ = 0.9;
 X = [hI hR hZ];
 
 % objective function
-z = -1:0.1:1;
-Bzh = mean(Bz(X, z));
-fobj = @(x)((1/Bzh)*(norm(Bz(x, z) - Bz(X, z)).*sqrt(inv(length(z)))));
-fobj2d = @(x)(norm(Bz2d(x, z) - Bz2d(X, z)));
+z = -1:0.01:1;
+fobj = @(x)((1/mean(Bz(X, z)))*(norm(Bz(x, z) - Bz(X, z))*sqrt(inv(length(z)))));
+fobj2d = @(x)((1/mean(Bz2d(X, z)))*(norm(Bz2d(x, z) - Bz2d(X, z))*sqrt(inv(length(z)))));
 bound1 = @(x)((x(1)+1)^2+(x(2)-1)^2+(x(3)-1)^2 -1.35^2);
 bound2 = @(x)(2*x(1)+x(2)-x(3));
 
 % simplex algorythm
 bounds = {};
-settings = struct('range', 1, 'step', 0.01, 'slices', 20, 'dimension', 2);
+settings = struct('range', 1, 'step', 0.01, 'slices', length(z), 'dimension', 3);
 stop_conditions = struct('maxFlips', 1000, 'minHalving', 1e-5, 'minMargin', 1e-5);
-start_conditions = struct('start', [0.4 0.1], 'length', 0.2);
-obj = NelderMeadMethod(fobj2d, bounds, stop_conditions, start_conditions, settings);
+start_conditions = struct('start', [0.1 0.1 0.4], 'length', 0.5);
+obj = NelderMeadMethod(fobj, bounds, stop_conditions, start_conditions, settings);
 
 % plot ideal minimum
-plot(X(1), X(2), '.', 'color', 'r', 'lineWidth', 4);
-% plot(X(1), X(2), X(3), '.', 'color', 'r', 'lineWidth', 4);
+% plot(X(1), X(2), '.', 'color', 'r', 'lineWidth', 4);
+plot3(X(1), X(2), X(3), 'x', 'color', 'g', 'lineWidth', 1.5);
 
 function res = Bz(X, z)
-    global mu0
     I = X(1);
     R = X(2);
     Z = X(3);
-    res = (mu0/2)*(((I*R.^2)./(((Z-z).^2+R.^2).^(3/2))) + ((I*R.^2)./(((Z+z).^2+R.^2).^(3/2))));
+    res = (I*R.^2)./(((Z-z).^2+R.^2).^(3/2)) + (I*R.^2)./(((Z+z).^2+R.^2).^(3/2));
 end
 
 function res = Bz2d(X, z)
